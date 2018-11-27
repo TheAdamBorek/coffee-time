9c0ba4ece3837bc9e2fcac7363a2d70b97ab7707
Adam Borek <adambor3k@gmail.com>
2018-11-27 16:16:17 +0100

    creates to_net_equest helper method

--- app/models/http_client.rb
+++ app/models/http_client.rb
@@ -1,14 +1,9 @@
 class HttpClient
   def post(request)
     Thread.start do
-      http             = Net::HTTP.new(request.url.hostname, request.url.port)
-      http.use_ssl     = true
-      net_request      = Net::HTTP::Post.new(request.url)
-      net_request.body = request.body.to_json
-      request.headers.each do |key, value|
-        net_request[key] = value
-      end
-      http.request(net_request)
+      http = Net::HTTP.new(request.url.hostname, request.url.port)
+      http.use_ssl = true
+      http.request(request.to_net_request)
     end
   end
 end
--- app/models/http_request.rb
+++ app/models/http_request.rb
@@ -1,10 +1,10 @@
 module HttpRequest
-  def options
-    headers = self.headers || Hash.new
-    hash = {headers: headers}
-    unless body.nil?
-      hash.merge({body: body})
+  def to_net_request
+    request      = Net::HTTP::Post.new(request.url)
+    request.body = request.body.to_json
+    request.headers.each do |key, value|
+      request[key] = value
     end
-    hash
+    request
   end
 end
