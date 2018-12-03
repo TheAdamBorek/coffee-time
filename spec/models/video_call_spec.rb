51efc9c48788209a760fd7f837b19066dd5cc74c
Adam Borek <adambor3k@gmail.com>
2018-12-03 13:08:48 +0100

    adds some errors validation specs

--- spec/models/video_call_spec.rb
+++ spec/models/video_call_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe VideoCall, type: :model do
+  shared_examples_for "raise_url_format_error" do
+    it "raises url format error" do
+      expect { subject }.to raise_error(NotURLFormatError)
+    end
+  end
+
   it 'has a default value from config of video call' do
     call = VideoCall.new
     expect(call.link).to eq "https://tests.com/config_redirect"
@@ -10,6 +16,18 @@ RSpec.describe VideoCall, type: :model do
     create :video_call
     call = VideoCall.last
     expect(call.link).to_not eq "https://tests.com/config_redirect"
+  end
+
+  describe "#update_link" do
+    context "it's not http or https" do
+      subject { VideoCall.update_link("ftp://link.com") }
+      it_behaves_like "raise_url_format_error"
+    end
+
+    context "it's not an URL" do
+      subject { VideoCall.update_link("a normal string") }
+      it_behaves_like "raise_url_format_error"
+    end
   end
 end
 
